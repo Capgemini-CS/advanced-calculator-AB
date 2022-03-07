a344b7a475b89f38ccdc952d49a0501a0c9d602d
AndreeaaBadea <andreea.badea@capgemini.com>
2022-03-08 00:24:06 +0200

    commit

--- test/com/calculator/operations/AdditionTest.java
+++ test/com/calculator/operations/AdditionTest.java
@@ -1,4 +1,16 @@
+package com.calculator.operations;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AdditionTest {
-  
+
+    Addition addition = new Addition();
+
+    @Test
+    void executeAddition() {
+        assertEquals(3, addition.calculate(1,2));
+    }
+
 }
--- test/com/calculator/operations/DivisionTest.java
+++ test/com/calculator/operations/DivisionTest.java
@@ -3,7 +3,7 @@ package com.calculator.operations;
 import com.calculator.exception.DivisionByZeroException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DivisionTest {
 
